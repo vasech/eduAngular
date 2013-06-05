55c3f9911c4abc3189db9409bd489184faf7b70f
vasech <vassili.echser@centralway.com>
2013-06-05 09:33:41 +0200

    Trying to set up service

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('appr', ['appr.controllers']).
+angular.module('appr', ['appr.controllers', 'apprSrvc']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     	when('/room0', {templateUrl: 'partials/room0.html', controller: 'roomCtrl'}).
--- app/js/services.js
+++ app/js/services.js
@@ -5,3 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
+
+var appr = angular.module('apprSrvc', []).factory('room', function(){
+  console.log("he");
+  return "hello";
+});
